3abe5fdc5188abdbfdc6789969845ea8b0ac40e8
Christine Johnson <crissyjohnson@christines-air.myfiosgateway.com>
2022-09-01 21:37:23 -0400

    add Network.tf

--- Nertwork.tf
+++ Nertwork.tf
@@ -2,7 +2,16 @@ resource "aws_vpc" "crissy_vpc" {
   cidr_block       = "10.0.0.0/16"
   instance_tenancy = "default"
 
-  tags = {
+  tags = {clear
     Name = "crissy_vpc"
+  }
+}
+resource "aws_subnet" "crissy_vpc_public_subnet" {
+  vpc_id     = aws_vpc.crissy_vpc.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "us-eat-1a
+
+  tags = {
+    Name = "Main"
   }
 }
